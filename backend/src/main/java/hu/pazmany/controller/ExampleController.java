d697e7d808e7f93cab09f0ca4c826c38aebb74e1
László Kuczik <laszlo.kuczik@shiwaforce.com>
2024-04-23 13:13:57 +0200

    + example controller, provides on /api/ok a OK String.

---
+++ backend/src/main/java/hu/pazmany/controller/ExampleController.java
@@ -0,0 +1,22 @@
+package hu.pazmany.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ExampleController {
+	private static final String OKAY_STRING = "OK";
+
+	/**
+	 * Provides an OK string
+	 *
+	 * @return An OK string
+	 */
+
+	@GetMapping("/ok")
+	public String ok() {
+		return OKAY_STRING;
+	}
+}
---
+++ backend/src/test/java/hu/pazmany/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.pazmany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
